ba33e5fc37721456710c825f6c6340e21f6cd9d0
Christopher Boczko <chris@furtherthinking.net>
2023-02-06 13:52:22 +0000

    intial commit with blazer framework

--- Source_Code/RecipeThesaurus/Pages/Privacy.cshtml.cs
+++ Source_Code/RecipeThesaurus/Pages/Privacy.cshtml.cs
@@ -1,19 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RecipeThesaurus.Pages;
-
-public class PrivacyModel : PageModel
+namespace RecipeThesaurus.Pages
 {
-    private readonly ILogger<PrivacyModel> _logger;
-
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public class PrivacyModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<PrivacyModel> _logger;
 
-    public void OnGet()
-    {
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
     }
 }
-
