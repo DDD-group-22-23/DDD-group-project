b3d35e24e9c0da720315293c8412ee50c4c3eadd
Christopher Boczko <chris@furtherthinking.net>
2023-02-07 12:19:58 +0000

    inital sql based login razor pages

---
+++ Source_Code/RecipeThesaurus/Models/DB/LoginByUsernamePassword.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+namespace RecipeThesaurus.Models.DB
+{
+    #region Login by username and password store procedure method.  
+    public class LoginByUsernamePassword
+    { 
+        /// <summary>  
+        /// Login by username and password store procedure method.  
+        /// </summary>  
+        /// <param name="usernameVal">Username value parameter</param>  
+        /// <param name="passwordVal">Password value parameter</param>  
+        /// <returns>Returns - List of logins by username and password</returns>  
+        public async Task<List<LoginByUsernamePassword>> LoginByUsernamePasswordMethodAsync(string usernameVal, string passwordVal)
+        {
+            // Initialization.  
+            List<LoginByUsernamePassword> lst = new List<LoginByUsernamePassword>();
+
+            try
+            {
+                // Settings.  
+                SqlParameter usernameParam = new SqlParameter("@username", usernameVal ?? (object)DBNull.Value);
+                SqlParameter passwordParam = new SqlParameter("@password", passwordVal ?? (object)DBNull.Value);
+
+                // Processing.  
+                string sqlQuery = "EXEC [dbo].[LoginByUsernamePassword] " +
+                                    "@username, @password";
+
+                lst = await this.FromSql(sqlQuery, usernameParam, passwordParam).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            // Info.  
+            return lst;
+        }
+
+        #endregion
+    }
+    }
---
+++ Source_Code/RecipeThesaurus/Models/LoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace RecipeThesaurus.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>  
+    /// Login view model class.  
+    /// </summary>  
+    public class LoginViewModel
+    {
+        #region Properties  
+
+        /// <summary>  
+        /// Gets or sets to username address.  
+        /// </summary>  
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        /// <summary>  
+        /// Gets or sets to password address.  
+        /// </summary>  
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        #endregion
+    }
+}
