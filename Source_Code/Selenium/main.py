ca49abde491efbcedd8ea9ad624eebd8bab615d7
CrimsonTome <crimsontome427@protonmail.com>
2023-02-25 20:32:39 +0000

    fix: firefox headless

--- Source_Code/Selenium/main.py
+++ Source_Code/Selenium/main.py
@@ -1,13 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
+driver.get("http://google.com/")
+print ("Headless Firefox Initialized")
+driver.quit()
