954dd066d33175ae20f8b7678ea5ca6167907f63
Christopher Boczko <chris@furtherthinking.net>
2023-02-26 18:09:29 +0000

    David's inital SQL design

---
+++ Source_Code/RecipeThesaurus-Database/CreateTables.sql
@@ -0,0 +1,436 @@
+CREATE DATABASE recipeAppDb;
+USE recipeAppDb;
+
+CREATE TABLE users (
+username varchar(20) NOT NULL,
+firstname varchar(20),
+lastname varchar(20),
+email varchar(50),
+PRIMARY KEY (username)
+);
+
+CREATE TABLE recipes (
+recipeId int NOT NULL,
+recipeName varchar(100) NOT NULL,
+recipeDescription varchar(200),
+recipeInstructions varchar(1000),
+recipeLikes int,
+imageURL varchar(100),
+recipeAuthor varchar(20),
+PRIMARY KEY (recipeId),
+FOREIGN KEY (recipeAuthor) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientLikes (
+username varchar(20) NOT NULL,
+ingredient varchar(20) NOT NULL,
+FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientDisikes (
+username varchar(20) NOT NULL,
+ingredient varchar(20) NOT NULL,
+FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userFridge (
+username varchar(20) NOT NULL,
+ingredient varchar(20) NOT NULL,
+FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE savedRecipes (
+username varchar(20) NOT NULL,
+recipeId int NOT NULL,
+FOREIGN KEY (username) REFERENCES users(username),
+FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+CREATE TABLE recipeIngredients (
+recipeId int NOT NULL,
+FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE DATABASE recipeAppDb;
+USE recipeAppDb;
+
+CREATE TABLE users (
+	username varchar(20) NOT NULL,
+	firstname varchar(20),
+	lastname varchar(20),
+	email varchar(50),
+	PRIMARY KEY (username)
+);
+
+CREATE TABLE recipes (
+	recipeId int NOT NULL,
+	recipeName varchar(100) NOT NULL,
+	recipeDescription varchar(200),
+	recipeInstructions varchar(1000),
+	recipeLikes int,
+	imageURL varchar(100),
+	recipeAuthor varchar(20),
+	PRIMARY KEY (recipeId),
+	FOREIGN KEY (recipeAuthor) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientLikes (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientDisikes (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userFridge (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE savedRecipes (
+	username varchar(20) NOT NULL,
+	recipeId int NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username),
+	FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+CREATE TABLE recipeIngredients (
+	recipeId int NOT NULL,
+	FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE DATABASE recipeAppDb;
+USE recipeAppDb;
+
+CREATE TABLE users (
+	username varchar(20) NOT NULL,
+	firstname varchar(20),
+	lastname varchar(20),
+	email varchar(50),
+	PRIMARY KEY (username)
+);
+
+CREATE TABLE recipes (
+	recipeId int NOT NULL,
+	recipeName varchar(100) NOT NULL,
+	recipeDescription varchar(200),
+	recipeInstructions varchar(1000),
+	recipeLikes int,
+	imageURL varchar(100),
+	recipeAuthor varchar(20),
+	PRIMARY KEY (recipeId),
+	FOREIGN KEY (recipeAuthor) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientLikes (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientDisikes (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userFridge (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE savedRecipes (
+	username varchar(20) NOT NULL,
+	recipeId int NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username),
+	FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+CREATE TABLE recipeIngredients (
+	recipeId int NOT NULL,
+	FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE DATABASE recipeAppDb;
+USE recipeAppDb;
+
+CREATE TABLE users (
+	username varchar(20) NOT NULL,
+	firstname varchar(20),
+	lastname varchar(20),
+	email varchar(50),
+	PRIMARY KEY (username)
+);
+
+CREATE TABLE recipes (
+	recipeId int NOT NULL,
+	recipeName varchar(100) NOT NULL,
+	recipeDescription varchar(200),
+	recipeInstructions varchar(1000),
+	recipeLikes int,
+	imageURL varchar(100),
+	recipeAuthor varchar(20),
+	PRIMARY KEY (recipeId),
+	FOREIGN KEY (recipeAuthor) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientLikes (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientDisikes (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userFridge (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE savedRecipes (
+	username varchar(20) NOT NULL,
+	recipeId int NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username),
+	FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+CREATE TABLE recipeIngredients (
+	recipeId int NOT NULL,
+	FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE DATABASE recipeAppDb;
+USE recipeAppDb;
+
+CREATE TABLE users (
+	username varchar(20) NOT NULL,
+	firstname varchar(20),
+	lastname varchar(20),
+	email varchar(50),
+	PRIMARY KEY (username)
+);
+
+CREATE TABLE recipes (
+	recipeId int NOT NULL,
+	recipeName varchar(100) NOT NULL,
+	recipeDescription varchar(200),
+	recipeInstructions varchar(1000),
+	recipeLikes int,
+	imageURL varchar(100),
+	recipeAuthor varchar(20),
+	PRIMARY KEY (recipeId),
+	FOREIGN KEY (recipeAuthor) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientLikes (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientDisikes (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userFridge (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE savedRecipes (
+	username varchar(20) NOT NULL,
+	recipeId int NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username),
+	FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+CREATE TABLE recipeIngredients (
+	recipeId int NOT NULL,
+	FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE DATABASE recipeAppDb;
+USE recipeAppDb;
+
+CREATE TABLE users (
+	username varchar(20) NOT NULL,
+	firstname varchar(20),
+	lastname varchar(20),
+	email varchar(50),
+	PRIMARY KEY (username)
+);
+
+CREATE TABLE recipes (
+	recipeId int NOT NULL,
+	recipeName varchar(100) NOT NULL,
+	recipeDescription varchar(200),
+	recipeInstructions varchar(1000),
+	recipeLikes int,
+	imageURL varchar(100),
+	recipeAuthor varchar(20),
+	PRIMARY KEY (recipeId),
+	FOREIGN KEY (recipeAuthor) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientLikes (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientDisikes (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userFridge (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE savedRecipes (
+	username varchar(20) NOT NULL,
+	recipeId int NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username),
+	FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+CREATE TABLE recipeIngredients (
+	recipeId int NOT NULL,
+	FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE DATABASE recipeAppDb;
+USE recipeAppDb;
+
+CREATE TABLE users (
+	username varchar(20) NOT NULL,
+	firstname varchar(20),
+	lastname varchar(20),
+	email varchar(50),
+	PRIMARY KEY (username)
+);
+
+CREATE TABLE recipes (
+	recipeId int NOT NULL,
+	recipeName varchar(100) NOT NULL,
+	recipeDescription varchar(200),
+	recipeInstructions varchar(1000),
+	recipeLikes int,
+	imageURL varchar(100),
+	recipeAuthor varchar(20),
+	PRIMARY KEY (recipeId),
+	FOREIGN KEY (recipeAuthor) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientLikes (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userIngredientDisikes (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE userFridge (
+	username varchar(20) NOT NULL,
+	ingredient varchar(20) NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE TABLE savedRecipes (
+	username varchar(20) NOT NULL,
+	recipeId int NOT NULL,
+	FOREIGN KEY (username) REFERENCES users(username),
+	FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
+
+CREATE TABLE recipeIngredients (
+	recipeId int NOT NULL,
+	FOREIGN KEY (recipeId) REFERENCES recipes(recipeId)
+);
