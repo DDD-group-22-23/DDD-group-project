6a6f5c0674f1193b2f860aff0870ca3b53e016c1
CrimsonTome <crimsontome427@protonmail.com>
2023-02-28 02:08:34 +0000

    feat: get started properly with selenium
    tweaked gitignore to not commit python artifacts, need to look into environment variables for login, dotenv wasn't working so regular environment variable handling will have to do. might be easier to setup in gh actions like that anyway

--- Source_Code/Selenium/main-desktop.py
+++ Source_Code/Selenium/main-desktop.py
@@ -1,10 +1,26 @@
+# import selenium, firefox options, keys to type special keys, by to search elements on the page
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
+
+# initialise options, not headless because this is the desktop testing, we'd like to see what is going on
+# setup the driver with firefox
 options = Options()
 options.headless = False
 driver = webdriver.Firefox(options=options)
+
+# go to our homepage and check that we're at the homepage
 driver.get("https://app.recipethesaurus.software/")
-# print ("Headless Firefox Initialized")
+assert "Home Page" in driver.title
+
+# go to secrets login page
+elem = driver.find_element(By.LINK_TEXT, "Secrets")
+elem.click()
+assert "Login" in driver.title
+
+
+
 input()
 driver.quit()
