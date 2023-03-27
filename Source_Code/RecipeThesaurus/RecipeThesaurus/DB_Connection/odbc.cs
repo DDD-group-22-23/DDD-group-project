71f6f42841f61938cb6ffa073840938baf8de483
CrimsonTome <crimsontome427@protonmail.com>
2023-03-27 13:34:42 +0100

    add boilerplate db connector, need connection string to test further

---
+++ Source_Code/RecipeThesaurus/RecipeThesaurus/DB_Connection/odbc.cs
@@ -0,0 +1,42 @@
+﻿//https://www.easysoft.com/developer/languages/csharp/ado-net-odbc.html
+
+
+using System.Data.Odbc;
+
+namespace RecipeThesaurus.DB_Connection
+{
+    public class odbc
+    {
+        OdbcConnection DbConnection = new OdbcConnection("DSN=CONNECTION_STRING_HERE");
+        DbConnection.Open();
+        OdbcCommand DbCommand = DbConnection.CreateCommand();
+
+        DbCommand.CommandText = "SELECT * FROM NATION";
+        OdbcDataReader DbReader = DbCommand.ExecuteReader();
+        int fCount = DbReader.FieldCount;
+        Console.Write(":");
+
+        for (int i = 0; i< fCount; i ++ )
+        {
+            String fName = DbReader.GetName(i);
+            Console.Write(fName + ":");
+        }
+        Console.WriteLine();
+    }
+
+    while (DbReader.Read())
+    {
+        Console.Write(":");
+        for (int i = 0; i < fCount; i++)
+        {
+            String col = DbReader.GetString(i);
+
+            Console.Write(col + ":");
+        }
+        Console.WriteLine();
+    }
+
+    DbReader.Close();
+    DbCommand.Dispose();
+    DbConnection.Close();
+}
