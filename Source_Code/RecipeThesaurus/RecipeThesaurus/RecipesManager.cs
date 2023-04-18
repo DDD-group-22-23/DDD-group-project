593cca98fd8eb6664d4d22fb12199d37853e19cb
Nikolai <nikolaivalkamo1@gmail.com>
2023-04-18 16:55:59 +0100

    Created sqlite script. Modified sql functions for sqlite

--- Source_Code/RecipeThesaurus/RecipeThesaurus/RecipesManager.cs
+++ Source_Code/RecipeThesaurus/RecipeThesaurus/RecipesManager.cs
@@ -7,67 +7,98 @@ namespace RecipeThesaurus
 {
     public class Recipe
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public int ID { get; set; }
-        public string Instructions { get; set; }
+        public int id;
+        public string name;
+        public string description;
+        public string instructions;
+        public int likes;
+        public string url;
+        public string author;
+        public List<int> ingredientsIdList = new List<int>();
 
-        public Recipe(int id, string name, string desc, string inst)
+        public Recipe(int pid, string pname, string pdesc, string pinst, int plikes, string purl, string pauthor, List<int> pingredientsIdList=null)
         {
-            ID = id;
-            Name = name;
-            Description = desc;
-            Instructions = inst;
+            id = pid;
+            name = pname;
+            description = pdesc;
+            instructions = pinst;
+            likes = plikes;
+            url = purl;
+            author = pauthor;
+            ingredientsIdList = pingredientsIdList;
+        }
+
+        public void Log()
+        {
+            string outline = $"id={id}\nname={name}\ndesc={description}\ninstructions={instructions}\nlikes={likes}\nurl={url}\nauthor={author}";
+            Console.WriteLine(outline);
         }
     }
 
     public class RecipesManager
     {
-        List<Recipe> recipes = new List<Recipe>();
+        public List<Recipe> recipes = new List<Recipe>();
         SqlConnection conn;
 
-
-        public RecipesManager(SqlConnection con)
+        public RecipesManager(SqlConnection con = null)
         {
             conn = con;
         }
 
-        // Adds a recipe to the currently loaded recipes
-        public void AddRecipe()
-        {
-
-            // Recipe newRec = new Recipe();
-        }
-
         // Gets recipe information then stores
-        public void CreateRecipe()
+        public void CreateRecipe(string pUsername)
         {
-            string name = string.Empty;
-            string desc = string.Empty;
-            string inst = string.Empty;
-            int ID = 0; // Set to next open num
-            // string username = current user open
-
-            string command = string.Empty;
-            // command = "INSERT INTO recipes VALUES (ID, name, desc, inst, 0, "url", username)"
+            int id = 0;
+            string name = " A";
+            string desc = " A";
+            string inst = " A";
+            string url = " A";
+            string username = " A";
+            string ingredients = " A";
+            // need to add ingredients
+            string fields = $"(recipeId, recipeName, recipeDescription, recipeInstructions, recipeLikes, imageURL, recipeAuthor)";
+            string values = $"({id}, '{name}', '{desc}', '{inst}', 0, '{url}', '{username}')";
+            string saveRecipe = $"INSERT INTO recipes {fields} VALUES {values}";
+            SqlCommand command = new SqlCommand(saveRecipe, conn);
+            command.ExecuteNonQuery();
         }
 
-        // Opens a recipe with its whole description
-        public void ViewRecipe()
+        // Reads recipes stored and loads them into a list
+        // As the total amount of recipes stored is small, all can be loaded in at once
+        public void GetRecipes()
         {
+            // Need to also select 'recipeIngredients' when its been added to the sql
 
+            string getRecipe = $"SELECT recipeId, recipeName, recipeDescription, recipeInstructions, recipeLikes, imageURL, recipeAuthor FROM recipes";
+            
+            SqlCommand command = new SqlCommand(getRecipe, conn);
+            //bool valid = false; Only needed if getting a specific recipe
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    //valid = true;
+
+                    int id = Convert.ToInt32(reader["recipeId"]);
+                    string name = reader["recipeName"].ToString();
+                    string desc = reader["recipeDescription"].ToString();
+                    string inst = reader["recipeInstructions"].ToString();
+                    int like = Convert.ToInt32(reader["recipeLikes"]);
+                    string url = reader["imageURL"].ToString();
+                    string auth = reader["recipeAuthor"].ToString();
+                    // string ingr = reader["recipeIngredients"].ToString();
+
+                    Recipe recipe = new Recipe(id, name, desc, inst, like, url, auth/*, ingr*/);
+                    recipes.Add(recipe);
+                }
+            }
+            // return valid; 
         }
 
-        // Basic view of a recipe during search etc
-        public void ViewOverview()
-        {
-
-        }
-
+        // Clears all stored recipes 
         public void ClearRecipes()
         {
             recipes.Clear();
         }
-
     }
 }
