a28b48e6c438059d681f027c0745c456b4bbdd3d
CrimsonTome <crimsontome427@protonmail.com>
2023-04-18 09:42:10 +0100

    amend

--- Source_Code/RecipeThesaurus-Database/Program.cs
+++ Source_Code/RecipeThesaurus-Database/Program.cs
@@ -8,19 +8,12 @@ class OdbcTest
     {
         
         //create an odbc connection
-        OdbcConnection conn = new OdbcConnection("Driver={ODBC Driver 18 for SQL Server}Server=tcp:recipethesaurus.database.windows.net,1433;Initial Catalog=RecipeThesaurus;Persist SecurityInfo=False;User ID=CloudSA3888a0df;Password=password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        OdbcConnection conn = new OdbcConnection("Driver={ODBC Driver 18 for SQL Server}Server=tcp:recipethesaurus.database.windows.net,1433;Initial Catalog=RecipeThesaurus;Persist SecurityInfo=False;User ID=CloudSA3888a0df;Password=TAKE-FROM-SECRET;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         conn.Open();
         //create an odbc command
         OdbcCommand DbCommand = conn.CreateCommand();
-        DbCommand.CommandText = "SELECT * FROM RecipeThesaurus";
-        //create an odbc datareader
-        OdbcDataReader reader = DbCommand.ExecuteReader();
-        //read the data
-        while (reader.Read())
-        {
-            Console.WriteLine(reader["STUFF HERE, DON'T KNOW WHAT YET"]);
-        }
-        //close the reader
+        
+
         reader.Close();
         //close the connection
         conn.Close();
