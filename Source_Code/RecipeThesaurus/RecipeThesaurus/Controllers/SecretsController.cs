0c7c8883e100ee66983a70da13f5c4b68a66b802
Nikolai <nikolaivalkamo1@gmail.com>
2023-04-23 21:49:00 +0100

    Made secrets version for the home functions

--- Source_Code/RecipeThesaurus/RecipeThesaurus/Controllers/SecretsController.cs
+++ Source_Code/RecipeThesaurus/RecipeThesaurus/Controllers/SecretsController.cs
@@ -18,10 +18,52 @@ namespace RecipeThesaurus.Controllers
             // Not implemented - To be removed? No need to look at individual recipes
         //    return View();
         //}
-	public IActionResult Index()
+	    public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult SaveRecipe()
+        {
+            DBManager man = new DBManager(false);
+            User user = man.userManager.getUserByUsername("david"); // chnage to cookeis username
+            int id = Convert.ToInt32(Request.Form["id"]);
+            man.recipesManager.SaveRecipe(id, user);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Saved()
+        {
+            // DBManager needs to be set somewhere higher because its being recreated everywhere
+
+            DBManager man = new DBManager(false);
+            string username = "david"; // change to cookies.username
+            User user = man.userManager.getUserByUsername(username);
+            man.recipesManager.GetRecipes();
+            List<string> like = user.savedRecipes;
+            List<Recipe>? recipes = man.recipesManager.GetRecipesIds(like);
+            ViewData["RecipeList"] = recipes;
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult CreateRecipe()
+        {
+            DBManager man = new DBManager(false);
+            string title = Request.Form["title"];
+            string description = Request.Form["description"];
+            string ingredients = Request.Form["ingredients"];
+            string instructions = Request.Form["instructions"];
+            string image = Request.Form["image"];
+            string username = "nikolai"; // Chnage to the cookie data username
+            man.recipesManager.GetRecipes(); // Needed to get the next int for id
+            man.recipesManager.CreateRecipe(title, description, ingredients, instructions, image, username);
+            return RedirectToAction("Index");
+        }
+
+
         public IActionResult Search()
         {
             // DBManager needs to be set somewhere higher because its being recreated everywhere
