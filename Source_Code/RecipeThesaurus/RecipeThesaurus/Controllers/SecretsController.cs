42e646979573ed2658bc8708265037d3bc9818ee
Nikolai <nikolaivalkamo1@gmail.com>
2023-04-25 00:52:49 +0100

    Changed to secrets controller. Test for username output

--- Source_Code/RecipeThesaurus/RecipeThesaurus/Controllers/SecretsController.cs
+++ Source_Code/RecipeThesaurus/RecipeThesaurus/Controllers/SecretsController.cs
@@ -20,6 +20,8 @@ namespace RecipeThesaurus.Controllers
         //}
 	    public IActionResult Index()
         {
+            string username = User.Claims.Where(x => x.Type == "preferred_username").ToString();
+            ViewData["username"] = username;
             return View();
         }
 
